8f436910946de0e3c898bc51b60f9277d79f97f3
Alex Wang <alex.wang@chromatiqa.org>
2016-12-10 18:46:10 -0800

    Add user validation tests

--- nightmarket/app/models/user.rb
+++ nightmarket/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
+	validates :name, presence: true
+	validates :uid, presence: true
+	validates :provider, presence: true
+	validates :email, presence: true
+	validates :oauth_token, presence: true
+	validates :oauth_expires_at, presence: true
+
 	def self.from_omniauth(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
 			user.provider = auth.provider
--- nightmarket/test/models/user_test.rb
+++ nightmarket/test/models/user_test.rb
@@ -1,7 +1,70 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without provider" do
+    user = User.new
+
+    user.uid = "abc"
+    user.name = "Bobby McBobberson"
+    user.email = "bob@bob.lawblog"
+    user.oauth_token = "abc123"
+    user.oauth_expires_at = Time.new(2016)
+
+    assert !user.save, "Saved the user without a provider"
+  end
+
+  test "should not save user without name" do
+    user = User.new
+    user.provider = "Google"
+    user.uid = "abc"
+    user.email = "bob@bob.lawblog"
+    user.oauth_token = "abc123"
+    user.oauth_expires_at = Time.new(2016)
+
+    assert !user.save, "Saved the user without a name"
+  end
+
+  test "should not save user without uid" do
+    user = User.new
+    user.provider = "Google"
+    user.name = "Bobby McBobberson"
+    user.email = "bob@bob.lawblog"
+    user.oauth_token = "abc123"
+    user.oauth_expires_at = Time.new(2016)
+
+    assert !user.save, "Saved the user without a uid"
+  end
+
+  test "should not save user without email" do
+    user = User.new
+    user.provider = "Google"
+    user.name = "Bobby McBobberson"
+    user.uid = "abc"
+    user.oauth_token = "abc123"
+    user.oauth_expires_at = Time.new(2016)
+
+    assert !user.save, "Saved the user without a email"
+  end
+
+  test "should not save user without oauth token" do
+    user = User.new
+    user.provider = "Google"
+    user.name = "Bobby McBobberson"
+    user.uid = "abc"
+    user.email = "milktea@milktea.io"
+    user.oauth_expires_at = Time.new(2016)
+
+    assert !user.save, "Saved the user without a oauth token"
+  end
+
+  test "should not save user without oauth expiry" do
+    user = User.new
+    user.provider = "Google"
+    user.name = "Bobby McBobberson"
+    user.uid = "abc"
+    user.email = "milktea@milktea.io"
+    user.oauth_token = "abc123"
+
+    assert !user.save, "Saved the user without a oauth expiry"
+  end
 end
