22ae18a85f3d89b643c6bd56085b4979c0227f2f
Alex Wang <alex.wang@chromatiqa.org>
2016-12-10 19:14:18 -0800

    Remove incorrect placeholder tests

--- nightmarket/test/controllers/visit_controller_test.rb
+++ nightmarket/test/controllers/visit_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class VisitControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get visit_index_url
-    assert_response :success
-  end
 
 end
